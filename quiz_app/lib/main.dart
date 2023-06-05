03793d2d3374d1034dca9853659fdfa0358713a3
Connor Nolan <connornolan15@gmail.com>
2023-06-04 20:24:39 -0700

    start screen challenge attempt

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -1,1 +1,42 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Quiz App',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: Scaffold(
+        body: Center(
+          child: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Colors.purple.shade900,
+                  Colors.purple.shade800
+                ]
+              )
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset('assets/images/quiz-logo.png'),
+                const Text('Learn Flutter the fun way!'),
+              ],
+            ),
+          )
+        )
+      ),
+    );
+  }
+}
