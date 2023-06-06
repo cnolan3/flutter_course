d7aee0421fd07dff4995ef2034388616f4877966
Connor Nolan <connornolandev@gmail.com>
2023-06-05 17:32:56 -0700

    move MyApp into a file, create question screen file, make start screen image transparent

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -1,36 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'package:quiz_app/start_screen.dart';
+import 'package:quiz_app/app.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Quiz App',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-      ),
-      home: Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [
-                Colors.purple.shade900,
-                Colors.purple.shade800
-              ]
-            )
-          ),
-          child: const StartScreen()
-        )
-      )
-    );
-  }
 }
--- quiz_app/lib/start_screen.dart
+++ quiz_app/lib/start_screen.dart
@@ -1,32 +1,30 @@
 import 'package:flutter/material.dart';
 
-class StartScreen extends StatelessWidget{
+class StartScreen extends StatelessWidget {
   const StartScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset('assets/images/quiz-logo.png', width: 300),
-          const Padding(
-            padding: EdgeInsets.all(50.0),
-            child: Text('Learn Flutter the fun way!', style: TextStyle(
-              color: Colors.white,
-              fontSize: 20
-              )),
-          ),
-          OutlinedButton.icon(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset(
+          'assets/images/quiz-logo.png',
+          width: 300,
+          color: const Color.fromARGB(150, 255, 255, 255),
+        ),
+        const Padding(
+          padding: EdgeInsets.all(50.0),
+          child: Text('Learn Flutter the fun way!',
+              style: TextStyle(color: Colors.white, fontSize: 20)),
+        ),
+        OutlinedButton.icon(
             onPressed: () {},
-            style: OutlinedButton.styleFrom(
-              foregroundColor: Colors.white
-            ),
+            style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
             icon: const Icon(Icons.arrow_right_alt),
-            label: const Text('Start Quiz')
-          )
-        ],
-      )
-    );
+            label: const Text('Start Quiz'))
+      ],
+    ));
   }
 }
