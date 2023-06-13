272c3676efb66c2bf638695cd5b5aadc48459b3f
Connor Nolan <connornolan15@gmail.com>
2023-06-13 14:21:43 -0700

    style question screen

--- quiz_app/lib/question_screen.dart
+++ quiz_app/lib/question_screen.dart
@@ -12,30 +12,40 @@ class QuestionScreen extends StatefulWidget {
 }
 
 class _QuestionScreenState extends State<QuestionScreen> {
+  var curQuestionIndex = 0;
+
+  void AnswerQuestion() {
+    setState(() {
+      curQuestionIndex++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    final currentQuestion = questions[0];
+    final currentQuestion = questions[curQuestionIndex];
 
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            currentQuestion.text,
-            style: const TextStyle(
-              color: Colors.white,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              textAlign: TextAlign.center,
+              currentQuestion.text,
+              style: const TextStyle(
+                color: Colors.white,
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          for (var answer in currentQuestion.answers)
-            AnswerButton(answer, () {}),
-//          AnswerButton('Answer 1', () {}),
-//          AnswerButton('Answer 2', () {}),
-//          AnswerButton('Answer 3', () {}),
-        ],
+            const SizedBox(
+              height: 30,
+            ),
+            for (var answer in currentQuestion.getShuffledAnswers())
+              AnswerButton(answer, AnswerQuestion),
+          ],
+        ),
       ),
     );
   }
