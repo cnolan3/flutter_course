1756d42a42630d118ea01d3b0fbadc56dfad33f2
Connor Nolan <connornolan15@gmail.com>
2023-06-27 19:44:21 -0700

    quiz app: make results screen scrollable

--- quiz_app/lib/results_screen.dart
+++ quiz_app/lib/results_screen.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:quiz_app/data/questions.dart';
+import 'package:quiz_app/questions_summary.dart';
 
 class ResultsScreen extends StatelessWidget {
   const ResultsScreen({super.key, required this.selectedAnswers});
@@ -23,17 +25,24 @@ class ResultsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final summaryData = getSummaryData();
+    final numTotalQuestions = questions.length;
+    final numCorrectAnswers = summaryData.where((data) {
+      return data['user_answer'] == data['correct_answer'];
+    }).length;
+
     return SizedBox(
         width: double.infinity,
         child: Container(
             margin: const EdgeInsets.all(40),
             child:
                 Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-              const Text('You answered X out of Y questions correctly.'),
+              Text(
+                  'You answered $numCorrectAnswers out of $numTotalQuestions questions correctly.'),
               const SizedBox(
                 height: 30,
               ),
-              const Text('List of answers and questions'),
+              QuestionsSummary(summaryData),
               const SizedBox(
                 height: 30,
               ),
